8dce29f03859a3cbd5db30d3122f0a791421d4f6
Li Haotian <2878348186@qq.com>
2022-09-19 12:15:55 +0800

    Create test.java

---
+++ src/test/java/test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class test {
+    @Test
+    public void testcat() {
+        Cat c1 = new Cat("Tom",3,6.0,25.0);
+        Cat c2 = new Cat();
+        c2.setName("加菲");
+        c2.setAge(5);
+        c2.setWeight(10.0);
+        c2.setHeight(22.0);
+        System.out.println(c1+"\n"+c2);
+    }
+}
